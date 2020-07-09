b06edb6e9eec6b9b88294bd37fc3c296285847e3
mlgmontage <kettleofrassel1@gmail.com>
2020-07-09 13:10:24 +0500

    login page

--- frontend/README.md
+++ frontend/README.md
@@ -1,1 +1,3 @@
 ## Frontend
+
+- Bootswatch (Flatly)
---
+++ frontend/scripts/login.js
@@ -0,0 +1,35 @@
+const AUTH_URL = `${API_URL}/auth`;
+
+$(() => {
+  $("#loginForm").submit((event) => {
+    event.preventDefault();
+
+    const name = $("#name").val();
+    const password = $("#password").val();
+
+    const user = { name, password };
+
+    console.log(user);
+
+    login(user)
+      .then((result) => {
+        console.log(result);
+        const $successMessage = $("#successMessage");
+        $successMessage.text(result.message);
+        $successMessage.show();
+      })
+      .catch((error) => {
+        console.log(error);
+        const $errorMessage = $("#errorMessage");
+        $errorMessage.text(error.responseJSON.error);
+        $errorMessage.show();
+        setTimeout(() => {
+          $errorMessage.hide(100);
+        }, 1000);
+      });
+  });
+});
+
+function login(user) {
+  return $.post(`${AUTH_URL}/login`, user);
+}
