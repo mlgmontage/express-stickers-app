92f9fed5aa8fd5cc390d607b9e4c9ccf0cca5b8e
mlgmontage <kettleofrassel1@gmail.com>
2020-07-09 13:39:36 +0500

    signing up page

--- frontend/scripts/signup.js
+++ frontend/scripts/signup.js
@@ -1,1 +1,36 @@
-console.log('hello')
+const AUTH_URL = `${API_URL}/auth`
+
+$(() => {
+  $('#signupForm').submit((event) => {
+    event.preventDefault()
+
+    const name = $('#name').val()
+    const password = $('#password').val()
+
+    const user = { name, password }
+
+    console.log(user)
+
+    signup(user)
+      .then((result) => {
+        console.log(result)
+        const $successMessage = $('#successMessage')
+        $successMessage.text(result.message)
+        $successMessage.show()
+        window.location = `/login.html`
+      })
+      .catch((error) => {
+        console.log(error)
+        const $errorMessage = $('#errorMessage')
+        $errorMessage.text(error.responseJSON.error)
+        $errorMessage.show()
+        setTimeout(() => {
+          $errorMessage.hide(100)
+        }, 1000)
+      })
+  })
+})
+
+function signup(user) {
+  return $.post(`${AUTH_URL}/signup`, user)
+}
